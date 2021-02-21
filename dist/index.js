24b59d09c487b25fc34db921f50b2894f5b31918
wusili <498282023@qq.com>
2021-02-21 20:45:44 +0800

    up

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7974" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8308" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -389,4 +389,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/YJKJ/AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=dist/index.js.map
+//# sourceMappingURL=index.js.map
